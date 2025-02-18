463fd4f7bf384af847655512c383824bf6e41162
Romain Le Métayer <romain.le-metayer@insa-rennes.fr>
2025-02-17 22:07:44 -0500

    Fixed missing args in artist models by making some optionals

--- src/MusicInfluenceNetwork/Models/artist.py
+++ src/MusicInfluenceNetwork/Models/artist.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
+from typing import Optional
 
+    
 @dataclass
 class Artist:
     name: str
     mbid: str
     url: str
-    begin: int
+    begin: Optional[int] = None
 
 
 @dataclass
 class SimilarArtist(Artist):
-    match: float
+    match: Optional[float] = None
 
