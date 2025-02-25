0b816dac29e8f7c85919112f48e2c8ea57b2d279
Romain Le Métayer <romain.le-metayer@insa-rennes.fr>
2025-02-25 18:47:02 -0500

    Fixed sql script

--- schema.sql
+++ schema.sql
@@ -2,16 +2,16 @@
 
 
 CREATE TABLE Artist (
-    artist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     mbid INTEGER UNIQUE,
     artist_name TEXT NOT NULL,
-    career_start_year INT,
-    career_end_year INT
+    career_start_year INTEGER,
+    career_end_year INTEGER
 );
 
 CREATE TABLE Genre (
     genre_id INTEGER PRIMARY KEY,
-    genre_name TEXT NOT NULL UNIQUE
+    genre_name VARCHAR(256) NOT NULL
 );
 
 CREATE TABLE ArtistGenre (
@@ -25,7 +25,7 @@ CREATE TABLE ArtistGenre (
 
 
 CREATE TABLE SimilarArtist (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     artist_id INTEGER NOT NULL,
     similar_artist_id INTEGER NOT NULL,
     FOREIGN KEY (artist_id) REFERENCES Artists(id),
