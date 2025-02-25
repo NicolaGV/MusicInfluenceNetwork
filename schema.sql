5f288a55a66f83956da996542a16350e5428f42e
Romain Le Métayer <romain.le-metayer@insa-rennes.fr>
2025-02-25 18:25:42 -0500

    Added sql schema

---
+++ schema.sql
@@ -0,0 +1,35 @@
+
+
+
+CREATE TABLE Artist (
+    artist_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    mbid INTEGER UNIQUE,
+    artist_name TEXT NOT NULL,
+    career_start_year INT,
+    career_end_year INT
+);
+
+CREATE TABLE Genre (
+    genre_id INTEGER PRIMARY KEY,
+    genre_name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE ArtistGenre (
+    artist_id INTEGER,
+    genre_id INTEGER,
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
+    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id),
+    PRIMARY KEY (artist_id, genre_id)
+);
+
+
+
+CREATE TABLE SimilarArtist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist_id INTEGER NOT NULL,
+    similar_artist_id INTEGER NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES Artists(id),
+    FOREIGN KEY (similar_artist_id) REFERENCES Artists(id),
+    lastFM_similary FLOAT NOT NULL,
+    spotify_similarity FLOAT NOT NULL
+);
