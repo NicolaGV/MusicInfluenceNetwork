8b8336fd4323d09544c1fc9c7d4a5df25d45a2bf
Romain Le Métayer <romain.le-metayer@insa-rennes.fr>
2025-03-09 12:50:44 -0400

    Updated models

--- src/MusicInfluenceNetwork/Models/artist.py
+++ src/MusicInfluenceNetwork/Models/artist.py
@@ -5,12 +5,42 @@ from typing import Optional
 @dataclass
 class Artist:
     name: str
-    mbid: str
-    url: str
-    begin: Optional[int] = None
+    artist_id: Optional[int] = None
+    mbid: Optional[str] = None
+    career_start_year: Optional[int] = None
+    career_end_year: Optional[int] = None
+    
+    def __hash__(self):
+        return hash(self.name)
+            
+    def __eq__(self, other):
+        return self.name == other.name
+    
+    @staticmethod
+    def from_musicbrainz(mb_json: dict) -> 'Artist':
+        begin_year = mb_json["life-span"].get("begin", None)
+        end_year = mb_json["life-span"].get("end", None)
+        
+        if begin_year:
+            begin_year = int(begin_year[:4]) # Only the year
+        if end_year:
+            end_year = int(end_year[:4]) # Only the year
+        
+        return Artist(mb_json["name"], mb_json["id"], begin_year, end_year)
+    
+    @staticmethod
+    def from_db_row(row: tuple) -> 'Artist':
+        return Artist(name=row[2], artist_id=row[0], mbid=row[1], career_start_year=row[3], career_end_year=row[4])
 
 
 @dataclass
 class SimilarArtist(Artist):
-    match: Optional[float] = None
+    last_fm_match: Optional[float] = None
+    spotify_match: Optional[float] = 0
+    
+    def __hash__(self):
+        return hash(self.name)
+            
+    def __eq__(self, other):
+        return self.name == other.name
 
